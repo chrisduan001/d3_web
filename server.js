f0138b379a13660e5de9d35ec0fb92cdffb3aad6
Chris <Chris@Chris-iMac.fios-router.home>
2018-01-24 12:13:35 -0500

    Set up https

--- server.js
+++ server.js
@@ -5,13 +5,20 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 1337;
 const app = express();
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
 app.use(express.static(__dirname));
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
-const server = http.createServer(app);
+const options = {
+    key: fs.readFileSync("./certificate/polygradient.key"),
+    cert: fs.readFileSync("./certificate/polygradient.crt"),
+    ca: fs.readFileSync("./certificate/polygradient.ca_bundle")
+};
+
+const server = https.createServer(options, app);
 
 server.listen(port);
